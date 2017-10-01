810c2c3025b24b495940dfb5bee5c2e52e664e09
Julius Suominen <julius.suominen@gmail.com>
2017-10-01 16:42:09 +0200

    content projection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PopupDemoComponent1 } from './popup-demo/popup.component';
 import { PopupDirective } from './popup-demo/popup.directive';
+import { MessageComponent } from './content-projection-demo/content-projection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PopupDemoComponent1,
-    PopupDirective
+    PopupDirective,
+    MessageComponent
   ],
   imports: [
     BrowserModule
